d7edcd7e55806592056b1844d905ddf68bd88cb7
Nguyễn Khắc Tiếp <nkt10102004@gmail.com>
2024-11-26 18:53:06 +0700

    bai8

---
+++ AimsProject/Source/hust/soict/hedspi/aims/cart/Cart/Cart.java
@@ -0,0 +1,127 @@
+package hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20; // Giới hạn 20 DVDs trong giỏ
+    private DigitalVideoDisc itemsOrdered[] = 
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+    // Thêm một DVD
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added.");
+        } else {
+            System.out.println("The cart is full.");
+        }
+    }
+
+ // Phương thức với mảng có thêm tham số đánh dấu
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList, boolean isArray) {
+        for (DigitalVideoDisc dvd : dvdList) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = dvd;
+                qtyOrdered++;
+                System.out.println("The disc has been added: " + dvd.getTitle());
+            } else {
+                System.out.println("The cart is full. Cannot add more discs.");
+                break;
+            }
+        }
+    }
+
+    // Phương thức với varargs
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
+        for (DigitalVideoDisc dvd : dvds) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = dvd;
+                qtyOrdered++;
+                System.out.println("The disc has been added: " + dvd.getTitle());
+            } else {
+                System.out.println("The cart is full. Cannot add more discs.");
+                break;
+            }
+        }
+    }
+
+
+    // Overloaded: Thêm hai DVDs cùng lúc
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            addDigitalVideoDisc(dvd1); // Thêm DVD đầu tiên
+        }
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            addDigitalVideoDisc(dvd2); // Thêm DVD thứ hai
+        }
+    }
+
+    // Xóa một DVD
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getTitle().equals(disc.getTitle())) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null; 
+                qtyOrdered--;
+                found = true;
+                System.out.println("The disc has been removed.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("The disc was not found in the cart.");
+        }
+    }
+
+    // Tính tổng chi phí
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+    
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
+        }
+        System.out.println("Total cost: " + totalCost() + " $");
+        System.out.println("***************************************************");
+    }
+
+    //Tim kiem theo ID
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("DVD found: " + itemsOrdered[i].toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD found with ID: " + id);
+        }
+    }
+
+    //Tim kiem theo tieu de
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("DVD found: " + itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD found with title: " + title);
+        }
+    }
+}
---
+++ AimsProject/Source/hust/soict/hedspi/test/cart/CartTest/CartTest.java
@@ -0,0 +1,40 @@
+package hust.soict.hedspi.test.cart.CartTest;
+import hust.soict.hedspi.aims.cart.Cart.Cart;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
+
+public class CartTest {
+    public static void main(String[] args) {
+        // Create a new cart
+        Cart cart = new Cart();
+
+        // Create new DVD objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Test the print method
+        cart.print();
+
+        // Test search by ID
+        System.out.println("\nSearching for DVD with ID 1:");
+        cart.searchById(1);
+        
+        System.out.println("\nSearching for DVD with ID 3:");
+        cart.searchById(3);
+
+        System.out.println("\nSearching for DVD with ID 5:");
+        cart.searchById(5);
+
+        // Test search by title
+        System.out.println("\nSearching for DVD with title 'Star Wars':");
+        cart.searchByTitle("Star Wars");
+
+        System.out.println("\nSearching for DVD with title 'Frozen':");
+        cart.searchByTitle("Frozen");
+    }
+}
